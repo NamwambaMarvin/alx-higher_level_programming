2a523025c486e50b42145f45d9ceb4efa38c4d31
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-06-12 14:36:05 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(list_int):
     for number in list_int:
-        print(number, end = '\n')
+        number = str(number)
+        print('{}'.format(number), end = '\n')
