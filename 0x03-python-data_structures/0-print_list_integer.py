d8e4bac0d21118b058b13af62aa2da9a2935c422
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-06-13 08:24:31 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for number in my_list:
-        print('{}'.format(number), end='\n')
+        print('{}'.format(number))
+print_list_integer([1,1,345,566,77])
