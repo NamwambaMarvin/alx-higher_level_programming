bbeabcbef80b187d8024cb03254b82956dadfa66
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-06-16 19:47:58 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    mul_mat = []
+
+    if len(matrix > 0):
+        for one in matrix[:]:
+            mul_mat.append(list(map(lambda x: x ** 2, one)))
+    return mul_mat
