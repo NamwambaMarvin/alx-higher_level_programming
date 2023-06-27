018aa01cede06de7a85e79f753f23e1b6dc63fcb
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-06-27 03:45:16 -0700

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for index in reversed(range(97, 123)):
+    if (index % 2 == 0):
+        print("{:c}".format(index), end='')
+    else:
+        print("{:c}".format(index - 32), end="")
