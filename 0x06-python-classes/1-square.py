7130e010fd4cd137809df3aea1771387beaad6ee
= <=>
2023-07-02 12:51:12 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square Class
+
+A class to form a square
+
+"""
+
+
+class Square:
+    """__init__
+
+    The __init__ method defines the initial values of the square
+
+    Attributes:
+        size (int): Initial size of the square
+
+    """
+    def __init__(self, size):
+        self.__size = size
