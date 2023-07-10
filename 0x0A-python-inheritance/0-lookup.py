1c4323776cfbcc39bb9ebd26b0695a10c1e716db
= <=>
2023-07-10 15:31:21 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Return a list of methods and modules
+"""
+
+
+def lookup(obj):
+    """
+    Returns a list of available methids and attributes
+    """
+    return dir(obj)
