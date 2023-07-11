32434b2ce9e393e6310c53c0beec2d05c51f7739
= <=>
2023-07-11 16:23:08 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that counts number of lines
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    Functio to count the numver of lines
+    """
+    with open(filename, encoding='utf-8') as f:
+        return len(f.readlines())
