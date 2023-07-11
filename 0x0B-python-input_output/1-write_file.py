3910e8901506fcf7419f458d32d7bec0944833f8
= <=>
2023-07-11 16:29:55 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,7 @@ Module that counts number of lines
 
 def number_of_lines(filename=""):
     """
-    Functio to count the numver of lines
+    Functnio to count the numver of lines
     """
     with open(filename, encoding='utf-8') as f:
         return len(f.readlines())
