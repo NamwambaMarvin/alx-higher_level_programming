62c8be37243bf14e218ac364d4c038e647112c34
= <=>
2023-07-11 16:19:35 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Module to read a file
+"""
 def read_file(filename=""):
+    """
+    Function that reads a file
+    """
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
