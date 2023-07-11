f438f6ebb7bd71c3c9be0e7f50f8d6bda1abce99
= <=>
2023-07-10 18:41:31 -0700

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle that inherits from the Base Geometry
+    """
+    def __init__(self, width, height):
+        """
+        Initialises variables
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
