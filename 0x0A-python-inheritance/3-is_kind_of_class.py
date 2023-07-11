156c8912a78d7756e10395f3ff69946c106fc6ce
= <=>
2023-07-10 17:49:59 -0700

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Checks for inheritance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that checks for inheritance
+    """
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
