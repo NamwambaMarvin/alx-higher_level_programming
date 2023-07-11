3a1dd05e2a5bbef4e6eef2ffca3f80c29f414bad
= <=>
2023-07-10 19:41:59 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+The square module( inherits from Rectangle)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    The square class
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area
+        """
+        return self.__size * self__size
+
+    def __str__(self):
+        return '[Square] ' + str(self.__width) + '/' + str(self.__height)
