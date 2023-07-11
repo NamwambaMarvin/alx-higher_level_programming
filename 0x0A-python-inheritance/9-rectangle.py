05f64b1600c4bd8fc11cea0d7123f86976e4de2c
= <=>
2023-07-10 19:13:03 -0700

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle(BaseGeometry):
         """
         Calculates the area
         """
-        return self.__width * self__height
+        return self.__width * self.__height
 
     def __str__(self):
         return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
