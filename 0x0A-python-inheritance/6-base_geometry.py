9cfd246f1081568b8ad1e7e042e276da6e1b2568
= <=>
2023-07-10 18:12:25 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+An empty class of BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class of base geometry module
+    """
+    def area(self):
+        """
+        Raises an exception about the implementation
+        of area
+        """
+        raise Exception("area() is not implemented")
