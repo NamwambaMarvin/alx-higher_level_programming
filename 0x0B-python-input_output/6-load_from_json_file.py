d846d47fd1e8bcac8fa6dc187aa30616e162a42b
= <=>
2023-07-14 19:28:56 -0700

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Module that creates a json object from a file
+"""
+from json import loads
+
+
+def load_from_json_file(filename):
+    """
+    Load a json object from a file
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return loads(f.read())
