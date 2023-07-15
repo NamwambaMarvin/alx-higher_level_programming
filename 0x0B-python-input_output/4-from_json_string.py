172d37daab6b5c7da25d3eb99c60e70dfef591e4
= <=>
2023-07-14 18:54:05 -0700

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module to convert json into an object
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function to convert json into an object
+    """
+    return json.loads(my_str)
