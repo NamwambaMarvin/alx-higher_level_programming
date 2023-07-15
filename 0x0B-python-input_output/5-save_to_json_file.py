92b441401c1ba3521971417ebe62c6b3ad8d09a2
= <=>
2023-07-14 19:02:24 -0700

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that stores an object to a text file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    toSave = json.loads(my_obj)
+    with open(filename, 'w') as f:
+        f.write(toSave)
