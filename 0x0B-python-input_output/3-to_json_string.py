681b606f52a8d3b209d82ebfce1cc2ab7a49776b
= <=>
2023-07-14 18:46:26 -0700

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that converts JSON to a string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Converts an object to a string
+    """
+    return json.dumps(my_obj)
