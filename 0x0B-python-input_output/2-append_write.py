38f5d12eff7d96258f4c6656d226017a17e9c5dd
= <=>
2023-07-14 18:39:06 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that appends to  a file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Function that appends to a file
+    """
+    with open(filename, 'a') as f:
+        return f.write(text)
