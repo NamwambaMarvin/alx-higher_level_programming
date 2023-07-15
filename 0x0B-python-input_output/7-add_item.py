1fed581d2e0b81820756e734e59612ba9a8b429b
= <=>
2023-07-14 20:17:18 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Modulw that loads, saves
+"""
+import os
+import sys
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+if os.path.exists('add_item.json'):
+    obj = load('add_item.json')
+else:
+    obj = []
+for i in range(1, len(sys.argv)):
+    obj.append(sys.argv[i])
+
+save(obj, 'add_item.json')
