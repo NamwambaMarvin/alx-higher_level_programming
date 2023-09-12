d1769940509139bfa7782c6f358651b98fc37fa9
Namwamba <marvinnamwamba@gmail.com>
2023-09-12 16:34:25 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Class that defines a Rectangle with widtha nd height atttributes
+
+module.exports = class Rectangle {
+	constructor (w, h)
+	{
+		this.width = w;
+		this.height = h;
+	}
+};
