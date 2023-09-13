3be9cd484fcdfeef9a53e5433b8c1171f555db09
Namwamba <marvinnamwamba@gmail.com>
2023-09-12 17:38:52 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+// Inherits from the previous program
+
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+	constructor (size) {
+		super (size, size);
+	}
+	double () {
+		super.double();
+	}
+	charPrint (c = 'X') {
+		super.print(c);
+	}
+}
