ebd9d1a7a7ea333895095e452a24f9235f7427aa
= <=>
2023-09-17 17:02:03 -0700

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+List all states from
+the `hbtn_0e_0_usa` database
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    '''
+    Access data and fetch the states
+    '''
+    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.excecute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
