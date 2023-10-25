0a8a4af6a30903dfc85547ffe6cf37eb0987f2f0
marvin <marvinnamwamba@gmail.com>
2023-10-25 05:54:38 +0300

    03

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/:id'.concat(argv[2]);
+const url = 'https://swapi-api.alx-tools.com/api/films/:id'.concat(process.argv[2]);
 
 request(url, function(_err, _err, body){
 	body = JSON.parse(body);
